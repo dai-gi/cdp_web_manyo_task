c86b7e6e2f7f883e6c526a5c01050e70a26275b1
Daisuke Ueda　上田 大輔 <daisuke.u524@gmail.com>
2022-11-20 17:14:05 +0900

    Add: バリデーションを設定

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -8,12 +8,13 @@ class TasksController < ApplicationController
   end
 
   def create
-    @task = Task.create(task_params)
-    redirect_to
-  end
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to tasks_path, notice: t('.created')
+    else
+      render :new
+    end
 
-	def show
-    @task = Task.find(params[:id])
   end
 
   def show
@@ -26,19 +27,21 @@ class TasksController < ApplicationController
 
   def update
     @task = Task.find(params[:id])
-    @task.update(task_params)
-    redirect_to tasks_path
+    if @task.update(task_params)
+      redirect_to tasks_path, notice: t('.updated')
+    else
+      render :edit
+    end
   end
 
   def destroy
     @task = Task.find(params[:id])
     @task.destroy
-    redirect_to tasks_path
+    redirect_to tasks_path, notice: t('.destroyed')
   end
 
   private
     def task_params
       params.require(:task).permit(:title, :content)
     end
-
 end
