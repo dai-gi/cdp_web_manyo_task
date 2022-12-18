6881ce67830aeb25973aeff99dd995b7f70e5272
Daisuke Ueda　上田 大輔 <daisuke.u524@gmail.com>
2022-12-19 06:56:06 +0900

    Modify: 優先度とステータスが異なったオブジェクトが生成されるように変更

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,17 @@
+priority = '高'
+status = '完了'
+
 50.times do |n|
   n += 1
-  Task.create(title: "タスク#{n}", content: "タスク#{n}を行う" ,created_at: '2022-11-10'  )
+
+  case n
+  when 20 then
+    priority = '中'
+    status = '着手中'
+  when 35 then
+    priority = '低'
+    status = '未着手'
+  end
+
+  Task.create(title: "タスク#{n}", content: "タスク#{n}を行う" ,created_at: '2022-11-10', deadline_on: '2022-11-30', priority: "#{priority}", status: "#{status}")
 end
