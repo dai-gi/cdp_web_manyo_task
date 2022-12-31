d8901924cf108174296afa140398c7d94dc3346a
Daisuke Ueda　上田 大輔 <daisuke.u524@gmail.com>
2022-12-31 11:46:41 +0900

    Modify: Add: 検索機能とソート機能のロジックをモデルに移行、statusカラムにインデックスを貼る

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,20 +1,6 @@
 class TasksController < ApplicationController
   def index
-    if params[:sort_deadline_on]
-      @tasks = Task.order(deadline_on: :ASC).page(params[:page])
-    elsif params[:sort_priority]
-      @tasks = Task.order(priority: :DESC).page(params[:page])
-    elsif params[:search]
-      if params[:search][:title].present? and params[:search][:status].present?
-        @tasks = Task.where("title LIKE ?", "%#{params[:search][:title]}%").where(status: params[:search][:status]).page(params[:page])
-      elsif params[:search][:title].present?
-        @tasks = Task.where("title LIKE ?", "%#{params[:search][:title]}%").page(params[:page])
-      elsif params[:search][:status].present?
-        @tasks = Task.where(status: params[:search][:status]).page(params[:page])
-      end
-    else
-      @tasks = Task.order(created_at: :DESC).page(params[:page])
-    end
+    @tasks = Task.choose_tasks_processing(processing_params).page(params[:page])
   end
 
   def new
@@ -57,5 +43,9 @@ class TasksController < ApplicationController
   private
     def task_params
       params.require(:task).permit(:title, :content, :deadline_on, :priority, :status)
+    end
+
+    def processing_params
+      params.fetch(:task, {}).permit(:sort_deadline_on, :sort_priority, :status, :title)
     end
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -16,4 +16,28 @@ class Task < ApplicationRecord
   def set_deadline_on
     deadline_on.strftime("%F")
   end
+
+  scope :choose_tasks_processing, -> (processing_params) do
+    normal(processing_params)
+    .search(processing_params)
+    .sort_deadline_on(processing_params[:sort_deadline_on])
+    .sort_priority(processing_params[:sort_priority])
+  end
+
+  scope :normal, -> (processing_params) { order(created_at: :DESC) if processing_params.blank? }
+
+  scope :search, -> (processing_params) {
+    if processing_params[:title].present? and processing_params[:status].present?
+      where("title LIKE ?", "%#{processing_params[:title]}%").where(status: processing_params[:status])
+    elsif processing_params[:title].present?
+      where("title LIKE ?", "%#{processing_params[:title]}%")
+    elsif processing_params[:status].present?
+      where(status: processing_params[:status])
+    end
+  }
+
+  scope :sort_deadline_on, -> (processing_params) { order(deadline_on: :ASC) if processing_params.present? }
+
+  scope :sort_priority, -> (processing_params) { order(priority: :DESC) if processing_params.present? }
+
 end
